aa4b876587e561b0bd3609fc66f581f78322e3af
Ivana Donchevska <ivana.donchevska77@gmail.com>
2023-01-13 02:00:35 +0200

    Delete some functions from calendar.h

--- newHopes/Calendar.h
+++ newHopes/Calendar.h
@@ -13,12 +13,8 @@ private:
 	Date currentDate;
 	vector<Event> events;
 	string startingDay = "Sun";
-
 	//is it possible to make it glogal somehow and change it
 public:
-	
-	void addEvent(); //to add event to file
-
 	
 	Calendar();
 	
@@ -26,32 +22,19 @@ public:
 	void setStartingDay(string);
 
 	void startMenu();
+
 	//helpers for date validation when calling addEvent 
 	bool doesEventExist(string);
-	bool isDateCorrect(Date);
-	bool isEndDateValid(Date, Date);
-	//FIX THEM TO WORK OK!!!
-
-
+	void addEvent(); //to add event to file
 
 	int FirstDayOfMonth(int, int);
 	int getNumberOfDays(int, int);
 	void setFirstDay();
 	void showCalendar();
 
-
 	//void showScedule(); //?
 	void listEvents(); //list all events in file
-	
 	void deleteEvent(); //delete event from file
-
-	void changeStartingDay();
-
-	//void specialRead();
-	void readMap();
-	void readFromFileInsertToVector();
-
-	//bool compare(Event&, Event&);
-	//string YYYY_MM_DD(string);
+	void changeStartingDay(); //fix it to work okey
 
 };
